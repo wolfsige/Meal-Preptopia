fa788a1dde0e58d7759ddc3288c5ccff678410b4
Wolf <d.wolftripp@gmail.com>
2022-03-06 15:40:26 -0800

    add ingredient to mealSchema

--- models/meal.js
+++ models/meal.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
 const mealSchema = new Schema({
-  name: {type: String, required: true,}
+  name: {type: String, required: true,},
+  ingredient: [{type: Schema.Types.ObjectId, ref: "Ingredient"}]
 })
 
 const Meal = mongoose.model('Meal', mealSchema)
